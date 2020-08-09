da70352e9042dc5f28f18a4b1d44e1132436ba39
bearomac <bighong0404@sina.com>
2020-08-10 01:33:56 +0800

    提交分库分表功能,  未完成

--- src/main/java/com/example/api/config/JpushConfiguration.java
+++ src/main/java/com/example/api/config/JpushConfiguration.java
@@ -3,12 +3,15 @@ package com.example.api.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
+
 @Configuration
 @ConfigurationProperties(prefix="jpush")
 public class JpushConfiguration {
 
     private String appId;
     private String secretKey;
+    private Map<String, Map<String, String>> key;
 
     public String getAppId() {
         return appId;
@@ -24,5 +27,13 @@ public class JpushConfiguration {
 
     public void setSecretKey(String secretKey) {
         this.secretKey = secretKey;
+    }
+
+    public Map<String, Map<String, String>> getKey() {
+        return key;
+    }
+
+    public void setKey(Map<String, Map<String, String>> key) {
+        this.key = key;
     }
 }
